7a544ace1d833be57c69545fba16c3823efa3e6b
Kenny Garcia <kennygarcia@syn-2603-8001-a900-d089-0000-0000-0000-1ac4.res6.spectrum.com>
2025-06-13 22:09:19 -0700

    Completed 1 and 2

--- CodePath/Unit2/Session1/p2.py
+++ CodePath/Unit2/Session1/p2.py
@@ -6,4 +6,26 @@ You are designing an app for your festival to help attendees have the best exper
 If the artist searched for does not exist in festival_schedule, return the dictionary {"message": "Artist not found"}.
 '''
 
+def get_artist_info(artist, festival_schedule):
+    for key in festival_schedule:
+        if artist == key:
+            return festival_schedule[artist]
+        else:
+            return { 'message': 'Artist not found' }
 
+# Example Usage:
+
+festival_schedule = {
+    "Blood Orange": {"day": "Friday", "time": "9:00 PM", "stage": "Main Stage"},
+    "Metallica": {"day": "Saturday", "time": "8:00 PM", "stage": "Main Stage"},
+    "Kali Uchis": {"day": "Sunday", "time": "7:00 PM", "stage": "Second Stage"},
+    "Lawrence": {"day": "Friday", "time": "6:00 PM", "stage": "Main Stage"}
+}
+
+print(get_artist_info("Blood Orange", festival_schedule)) 
+print(get_artist_info("Taylor Swift", festival_schedule))  
+
+# Example Output:
+
+# {'day': 'Friday', 'time': '9:00 PM', 'stage': 'Main Stage'}
+# {'message': 'Artist not found'}
