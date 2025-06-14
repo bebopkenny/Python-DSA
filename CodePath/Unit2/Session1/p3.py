359371476269aaa552ef66bf8007a05983325267
Kenny Garcia <kennygarcia@syn-2603-8001-a900-d089-0000-0000-0000-1ac4.res6.spectrum.com>
2025-06-13 22:25:47 -0700

    File 3 completed

---
+++ CodePath/Unit2/Session1/p3.py
@@ -0,0 +1,22 @@
+'''
+Problem 3: Ticket Sales
+
+A dictionary ticket_sales is used to map ticket type to number of tickets sold. Return the total number of tickets of all types sold.
+'''
+
+def total_sales(ticket_sales):
+    total = 0
+    for key in ticket_sales.values(): # Access to the values
+        total += key
+    return total
+
+
+# Example Usage:
+
+ticket_sales = {"Friday": 200, "Saturday": 1000, "Sunday": 800, "3-Day Pass": 2500}
+
+print(total_sales(ticket_sales))
+
+# Example Output:
+
+# 4500
