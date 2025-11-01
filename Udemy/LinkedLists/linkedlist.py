9176aed2f583204d17c0f1d26a0053a5bd6fe89c
Kenny Garcia <kennygarcia15@csu.fullerton.edu>
2025-11-01 16:07:05 -0700

    Set value, insert, and remove are done

--- Udemy/LinkedLists/linkedlist.py
+++ Udemy/LinkedLists/linkedlist.py
@@ -45,7 +45,61 @@ class LinkedList:
             
 
         def prepend(self, value):
-            pass
+            new_node = Node(value)
+            if self.length == 0:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                new_node.next = self.head
+                self.head = new_node
+            self.length += 1
+            return True
+        
+        def pop_first(self):
+            if self.length == 0:
+                return None
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.lenght -= 1
+            if self.lenght == 0:
+                self.tail = None
+            return temp # returning the item we removed from LL
+        
+        def get(self, index):
+            if index < 0 or index >= self.lenght:
+                return None
+            temp = self.head
+            for _ in range(index):
+                temp = self.next
+            return temp.value
+                
+        def set_value(self, index, value):
+            if index < 0 or index >= self.length:
+                return None
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            temp.value = value
+            return True
 
         def insert(self, index, value):
-            pass
+            if index > 0 or index > self.length:
+                return None
+            new_node = Node(value)
+            if index == 0:
+                self.head = new_node
+                self.tail = new_node
+            prev = self.head
+            for _ in range(index - 1): # before the next index
+                prev = prev.next
+            new_node.next = prev.next
+            prev.next = new_node
+            if index == self.length: # if it is at the tail
+                self.tail = new_node
+            self.length += 1
+            return True
+        
+        def remove(self, index): 
+
+
